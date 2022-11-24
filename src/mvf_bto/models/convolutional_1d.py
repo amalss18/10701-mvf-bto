d892ef1ad58dcf2149e3f76c0a8b8b78ef79d5d6
anoushkabhutani <anoushkabhutani@gmail.com>
2022-11-24 17:30:25 -0500

    Update `Convolutional1D` model

--- src/mvf_bto/models/convolutional_1d.py
+++ src/mvf_bto/models/convolutional_1d.py
@@ -6,29 +6,32 @@ class Convolutional1D(tf.keras.Model):
         self,
         input_shape,
         n_outputs,
-        conv_filters=64,
-        conv_kernel_size=2,
+        conv_filters_1=64,
+        conv_kernel_size_1=8,
         pool_size=2,
-        dense_units=64,
+        dense_units=128,
     ):
         super().__init__()
-        self.conv = tf.keras.layers.Conv1D(
-            filters=conv_filters,
-            kernel_size=conv_kernel_size,
+        self.conv_1 = tf.keras.layers.Conv1D(
+            filters=conv_filters_1,
+            kernel_size=conv_kernel_size_1,
             activation="relu",
             input_shape=input_shape,
         )
 
-        self.pooling = tf.keras.layers.MaxPooling1D(
+        self.pooling_1 = tf.keras.layers.MaxPooling1D(
             pool_size=pool_size,
         )
+
         self.flatten = tf.keras.layers.Flatten()
-        self.dense = tf.keras.layers.Dense(units=dense_units)
+        self.dense = tf.keras.layers.Dense(units=dense_units, activation="leaky_relu")
+        self.dense_2 = tf.keras.layers.Dense(units=50, activation="tanh")
         self.output_layer = tf.keras.layers.Dense(units=n_outputs)
 
     def call(self, inputs):
-        x = self.conv(inputs)
-        x = self.pooling(x)
+        x = self.conv_1(inputs)
+        x = self.pooling_1(x)
         x = self.flatten(x)
         x = self.dense(x)
+        x = self.dense_2(x)
         return self.output_layer(x)
