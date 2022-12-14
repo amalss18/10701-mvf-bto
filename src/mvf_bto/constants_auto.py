fe20baf0630d9c3ded7fcfce19f5215533d96881
Amal S Sebastian <amalssebastian@gmail.com>
2022-12-14 14:06:38 -0500

    Duplicate old files for fixing autoregression

---
+++ src/mvf_bto/constants_auto.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+# DATA NORMALIZATION
+VOLTAGE_MIN = 1.9
+VOLTAGE_MAX = 3.5
+TEMPERATURE_MIN = 24
+TEMPERATURE_MAX = 38
+MAX_CYCLE = 2300
+
+# PREPROCESSING
+# Constant Current 4C Discharge current is approx 4A
+# Used to split discharge step from full cycle in preprocessing
+MAX_DISCHARGE_CURRENT = -3.98
+MIN_DISCHARGE_CURRENT = -4.05
+
+# charging current
+MIN_CHARGE_CURRENT = 0.0
+
+# INTERPOLATION
+# Discharge curve is interpolated over these capacities
+# More points in non-linear regions
+REFERENCE_DISCHARGE_CAPACITIES = [
+    0.00,
+    0.025,
+    0.075,
+    0.1,
+    0.2,
+    0.3,
+    0.4,
+    0.5,
+    0.6,
+    0.8,
+    0.85,
+    0.9,
+    0.95,
+    0.975,
+    0.98,
+    0.985,
+    0.99,
+    0.995,
+    0.998,
+    1.0,
+]
+
+# REFERENCE CHARGE CAPACITIES: DIFFERENT FROM DISCHARGE
+# THREE STEPS OF CHARGING REQUIRES NON-UNIFORM SPLIT OF INTERPOLATION POINTS
+Qc_eval = np.linspace(0,0.1,21)
+Qc_eval = np.append(Qc_eval, np.linspace(0.1,0.8,8))
+Qc_eval = np.append(Qc_eval, np.linspace(0.8,0.85,11))
+Qc_eval = np.append(Qc_eval, np.linspace(0.85,0.98,14))
+Qc_eval = np.append(Qc_eval, np.linspace(0.98,1.00,2))
+REFERENCE_CHARGE_CAPACITIES=Qc_eval
---
+++ src/mvf_bto/models/baseline_lstm_auto.py
@@ -0,0 +1,87 @@
+import tensorflow as tf
+
+class BaselineLSTM(tf.keras.Model):
+    def __init__(
+            self,
+            batch_input_shape,
+            n_outputs,
+            nf_steps=1,
+            lstm1_units=32,
+            lstm2_units=16,
+            dense1_units=16,
+            dense2_units=8,
+    ):
+        super().__init__()
+        self.lstm_1 = tf.keras.layers.LSTM(
+            units=lstm1_units,
+            return_sequences=True,
+            stateful=True,
+            batch_input_shape=batch_input_shape,
+        )
+        self.lstm_2 = tf.keras.layers.LSTM(
+            units=lstm2_units,
+            return_sequences=False,
+            stateful=False,
+        )
+        self.dense_1 = tf.keras.layers.Dense(units=dense1_units, activation="relu")
+        self.dense_2 = tf.keras.layers.Dense(units=dense2_units)
+        self.dense_3 = tf.keras.layers.Dense(units=nf_steps*n_outputs)
+        self.output_layer = tf.keras.layers.Reshape([nf_steps, n_outputs])
+
+    def call(self, inputs):
+        x = self.lstm_1(inputs)
+        x = self.lstm_2(x)
+        x = self.dense_1(x)
+        x = self.dense_2(x)
+        x = self.dense_3(x)
+        return self.output_layer(x)
+
+
+class LSTMAutoregression(tf.keras.Model):
+    def __init__(
+            self,
+            n_outputs,
+            nf_steps=1,
+            lstm1_units=32,
+            dense1_units=32,
+            dense2_units=8,
+            out_steps = 5
+    ):
+        super().__init__()
+        self.out_steps = out_steps
+        self.lstm_cell = tf.keras.layers.LSTMCell(lstm1_units)
+        self.lstm_rnn = tf.keras.layers.RNN(self.lstm_cell, return_state=True)
+
+        self.dense_1 = tf.keras.layers.Dense(units=dense1_units, activation="relu")
+        self.dense_2 = tf.keras.layers.Dense(units=dense2_units)
+        self.dense_3 = tf.keras.layers.Dense(units=nf_steps*n_outputs)
+    
+    def warmup(self, inputs):
+        x, *state = self.lstm_rnn(inputs)
+
+        x = self.dense_1(x)
+        x = self.dense_2(x)
+        prediction = self.dense_3(x)
+
+        return prediction, state        
+
+    def call(self, inputs, training=None):
+        predictions = []
+        prediction, state = self.warmup(inputs)
+
+        predictions.append(prediction)
+
+        for n in range(1, self.out_steps):
+            x = prediction
+
+            x, state = self.lstm_cell(x, states=state, training=training)
+            x = self.dense_1(x)
+            x = self.dense_2(x)
+            prediction = self.dense_3(x)
+ 
+            predictions.append(prediction)
+
+        predictions = tf.stack(predictions)
+        predictions = tf.transpose(predictions, [1, 0, 2])
+
+        return predictions
